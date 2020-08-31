72a9f693bffee4bbdd9634e117edd0ab2b2c0383
Fatih <mfatihinan@gmail.com>
2020-08-31 00:01:09 -0400

    Navbar is created

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 // @Component is a decorator
 @Component({
@@ -6,8 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: any[];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: any) => {
+        this.products = response.data;
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
   // Html'de variable'in bu sekilde kullanilmasina {{ title }} => interpolation deniyor
 }
